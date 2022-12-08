1fb76542384292d68d2431f1d1854e99126e592b
coffee-marios <mark_theo@yahoo.com>
2022-12-08 13:25:06 +0200

    Make it possible to draw

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 "use strict"
 
+let clicked = false;
 
 const container = document.getElementById("container");
+container.addEventListener("mousedown", () => {clicked = true;}, false);
+container.addEventListener("mouseup", () => {clicked = true;}, false);
+
+
+
 
 
 
 let maxGrid = 16 * 16;
 
 for (let i=0; i<maxGrid; i++){
+    
+    
     const newDiv = document.createElement("div");
+
     newDiv.classList.add("divStyle");
     container.append(newDiv);
-    console.log(i);
+    newDiv.addEventListener("mouseover", mouseOver, false);
+   
     
 
 }
+
+function mouseOver(event){
+    if(clicked){
+        event.target.style.backgroundColor = "black";
+    }
+    
+}
+
+//container.style.gridTemplateColumns = "repeat(10, 1fr)";
+
+
+  
+ 
+
+
+
+
+
+
+
 
